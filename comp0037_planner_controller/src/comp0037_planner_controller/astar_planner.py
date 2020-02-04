3ec732fc18770d1b6d98049983c4a311013dc204
Walter Wu <walterwuyan@gmail.com>
2020-02-04 15:24:48 +0000

    Add other heuristics

--- comp0037_planner_controller/src/comp0037_planner_controller/astar_planner.py
+++ comp0037_planner_controller/src/comp0037_planner_controller/astar_planner.py
@@ -10,6 +10,10 @@ import rospy
 
 class AstarPlanner(DijkstraPlanner):
 
+    def __init__(self, title, occupancyGrid, heuristic = "constant"):
+        DijkstraPlanner.__init__(self, title, occupancyGrid)
+        self.heuristic = heuristic
+
     def pushCellOntoQueue(self, cell):
         cell.pathCost = self.getPathEndingAtCell(cell).travelCost + self.computeHeuristic(cell)
 
@@ -20,7 +24,22 @@ class AstarPlanner(DijkstraPlanner):
         self.priorityQueue.append(cell)
 
     def computeHeuristic(self,cell):
-        return 0
+        if self.heuristic == "constant":
+            return 10
+
+        if self.heuristic == "euclidean":
+            return math.sqrt((cell.coords[0] - self.goal.coords[0])**2 + (cell.coords[1] - self.goal.coords[1])**2)
+        
+        k_coords = cell.coords
+        G_coords = self.goal.coords
+        if self.heuristic == "octile":
+            result = max(abs(k_coords[0]-G_coords[0]),abs(k_coords[1]-G_coords[1]))\
+            + (math.sqrt(2)-1) * min(abs(k_coords[0]-G_coords[0]),abs(k_coords[1]-G_coords[1]))
+            return result
+
+        if self.heuristic == "manhattan":
+            
+            return abs(k_coords[0]-G_coords[0])+abs(k_coords[1]-G_coords[1])
 
     # overwrite
     def search(self, startCoords, goalCoords):
